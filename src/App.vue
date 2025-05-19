7463372639780147821689952c742fd009b8d471
sino <2077450408@qq.com>
2025-05-19 17:47:10 +0800

    页面布局拆分

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
   <div id="app">
-  <el-button type="primary">这是一个按钮</el-button>
+  <index></index>
   </div>
 </template>
 
 <script>
 
 
+import Index from "@/components/index.vue";
+
 export default {
   name: 'App',
   components: {
-
+    Index
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/index.vue
@@ -0,0 +1,43 @@
+<script>
+import MyAside from "@/components/MyAside.vue";
+import MyHeader from "@/components/MyHeader.vue";
+import MyMain from "@/components/MyMain.vue";
+
+export default {
+name: "IndexPage",
+  components: {MyAside,MyHeader,MyMain}
+}
+</script>
+
+<template>
+  <el-container style="height: 100%; border: 1px solid #eee">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246);height: 100%">
+    <MyAside :tableData="tableData"></MyAside>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px;">
+    <MyHeader></MyHeader>
+      </el-header>
+
+      <el-main>
+          <MyMain></MyMain>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped>
+
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+.el-main{
+  padding: 5px;
+}
+.el-aside {
+  color: #333;
+}
+</style>
