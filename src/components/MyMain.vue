72db407eab2677c972051bfa83cc3a3a556b983e
sino <2077450408@qq.com>
2025-05-20 13:43:26 +0800

    列表的基本展示

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -2,13 +2,8 @@
 export default {
 name: "MyMain",
   data() {
-    const item = {
-      date: '2016-05-02',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    };
     return {
-      tableData: Array(20).fill(item)
+      tableData: []
     }
   },
   methods: {
@@ -20,6 +15,11 @@ name: "MyMain",
     loadPost(){
     this.$axios.post( this.$httpUrl+'/user/listP',{}).then(res =>res.data).then((res) => {
       console.log(res)
+      this.tableData = res
+      if (res.code==200){
+        this.tableData = res.data
+      }else
+        alert('获取数据失败')
     })
     }
   },
@@ -31,12 +31,37 @@ name: "MyMain",
 </script>
 
 <template>
-  <el-table :data="tableData">
-    <el-table-column prop="date" label="日期" width="140">
+  <el-table :data="tableData"
+  :header-cell-style="{background: '#2fddfa'}"
+            border
+  >
+    <el-table-column prop="id" label="ID" width="60">
     </el-table-column>
-    <el-table-column prop="name" label="姓名" width="120">
+    <el-table-column prop="name" label="姓名" width="180">
     </el-table-column>
-    <el-table-column prop="address" label="地址">
+    <el-table-column prop="no" label="账号" width="180">
+    </el-table-column>
+    <el-table-column prop="age" label="年龄" width="180">
+    </el-table-column>
+    <el-table-column prop="phone" label="电话" width="180">
+    </el-table-column>
+    <el-table-column prop="sex" label="性别" width="180">
+      <template slot-scope="scope">
+        <el-tag
+            :type="scope.row.sex === 1 ? 'primary' : 'success'"
+            disable-transitions>{{scope.row.sex === 1 ? '男' : '女'}}</el-tag>
+      </template>
+    </el-table-column>
+    <el-table-column prop="roleId" label="角色" width="180">
+      <template slot-scope="scope">
+        <el-tag
+            :type="scope.row.roleId === 0 ? 'danger' : (scope.row.roleId === 1 ? 'primary' : 'success')"
+            disable-transitions>{{scope.row.roleId === 0 ? '超级管理员' : (scope.row.roleId === 1 ? '管理员' : '普通用户')}}</el-tag>
+      </template>
+    </el-table-column>
+    <el-table-column prop="operate" label="操作" width="180">
+      <el-button size="small" type="success">编辑</el-button>
+      <el-button size="small" type="danger">删除</el-button>
     </el-table-column>
   </el-table>
 </template>
