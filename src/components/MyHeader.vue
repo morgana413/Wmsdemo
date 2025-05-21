9ea2b185daf3560df37579643686ed0c7f797877
sino <2077450408@qq.com>
2025-05-21 13:48:50 +0800

    退出登录的实现

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
   name: "MyHeader",
+  data(){
+    return {
+      user:JSON.parse(sessionStorage.getItem("CurUser")),
+    }
+  },
   props: {
     icon: String
   },
@@ -10,6 +15,25 @@ export default {
     },
     logout(){
       console.log('log_out')
+      this.$confirm('您确定要退出登录吗？','提示',{
+        confirmButtonText:'确定',
+        type:"warning",
+        center:true
+      })
+          .then(() => {
+            this.$message({
+              type:"success",
+              message:'退出登陆成功'
+            })
+            this.$router.push('/')
+            sessionStorage.clear()
+          })
+          .catch(() => {
+            this.$message({
+              type:"info",
+              message:'已取消退出登陆'
+            })
+          });
     },
     collapse(){
       this.$emit('doCollapse')
@@ -27,7 +51,7 @@ export default {
       <span>欢迎来到仓库管理系统</span>
     </div>
   <el-dropdown>
-    <span>王小虎</span> <i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+    <span>{{ user.name }}</span> <i class="el-icon-arrow-down" style="margin-left: 5px"></i>
     <el-dropdown-menu slot="dropdown">
       <el-dropdown-item @click.native="toUser">个人中心</el-dropdown-item>
       <el-dropdown-item @click.native="logout">退出登录</el-dropdown-item>
